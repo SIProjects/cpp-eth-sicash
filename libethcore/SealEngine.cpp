27f16d06c3a9d07803ef8d832a183f977f0f9c8b
SITech <49570885+SIProjects@users.noreply.github.com>
2020-08-23 20:14:42 +0100

    Update SealEngine.cpp

--- libethcore/SealEngine.cpp
+++ libethcore/SealEngine.cpp
@@ -171,12 +171,12 @@ SealEngineFace* SealEngineRegistrar::create(ChainOperationParams const& _params)
 
 EVMSchedule const& SealEngineBase::evmSchedule(u256 const& _blockNumber) const
 {
-	//////////////////////////////////////////////////////// // qtum
+	//////////////////////////////////////////////////////// // sicash
 	if (u256(0) == chainParams().EIP158ForkBlock && 
 		u256(0) == chainParams().EIP150ForkBlock &&
 		u256(0) == chainParams().homesteadForkBlock &&
 		 false  == chainParams().allowFutureBlocks){
-			return getQtumSchedule();
+			return getSicashSchedule();
 	}
 	////////////////////////////////////////////////////////
 	return chainParams().scheduleForBlockNumber(_blockNumber);
